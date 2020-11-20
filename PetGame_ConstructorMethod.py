a5340d33b1cc8def8a6151f6baf8a2889364ead6
Logan <55439289+afrench14@users.noreply.github.com>
2020-11-20 14:07:54 +0000

    inital session, constructor code

--- PetGame_ConstructorMethod.py
+++ PetGame_ConstructorMethod.py
@@ -1,1 +1,16 @@
+#Construtor code to the pet game, templated from the crop simulation constructor
 
+class Pet:
+
+    #constructor
+    def __init__(self, petName, petType):
+        #setting attributes with an initial value
+        self.__petName = petName
+        self.__petType = petType
+        self.__bored = 0
+        self.__hunger = 0
+        self.__intelligence = 0
+        #the above attributes are prefixed with an underscore to indicate
+        #that they cannot be accessed from outside the class
+        #this is making them hem 'private'
+        #meaing you need to use a method to access them, encapsulating them
