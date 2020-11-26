1135e677abaf383d0364b26a5be97632bf14b1bd
Logan <55439289+afrench14@users.noreply.github.com>
2020-11-26 13:51:38 +0000

    updated funion

--- PetGame_MainCode.py
+++ PetGame_MainCode.py
@@ -7,10 +7,25 @@ class Pet:
         self.bored = 0
         self.hunger = 0
         self.intelligence = 0
-    
+
     def showHunger():
-        print(self.hunger)
-    
+        print(self.petName, "has", self.hunger, "hunger points remaining")
+        if self.hunger == 100:
+            print(self.petName, "has", self.hunger, "hunger points remaining and is full, good job")
+        elif  70 =< self.hunger < 100:
+            print(self.petName, "has", self.hunger, "hunger points remaining and is satisfied")
+        elif 50 =< self.petName < 70:
+            print(self.petName, "has", self.hunger, "hunger points remaining and is getting hungry")
+        elif 20 =< self.petName < 50:
+            print(self.petName, "has", self.hunger, "hunger points remaining and is a little hungry")
+        elif 10 =< self.petName < 20:
+            print(self.petName, "has", self.hunger, "hunger points remaining and is extremely hungry, feed asap")
+        elif 0 < self.petName < 10:
+            print(self.petName, "has", self.hunger, "hunger points remaining and will soon starve, feed immediately")
+        #include 'dead' here
+        else:
+            print("error returning", self.petNme + "'s hunger, sorry :(")
+
     def outputGreeting():
         print("hello, i am", self.petName, "i'm a", self.petType)
 
