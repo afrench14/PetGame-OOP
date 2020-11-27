de97f195a44a5607605f36e33a4601720bf232bf
Logan <55439289+afrench14@users.noreply.github.com>
2020-11-27 13:38:18 +0000

    Changes and fix to self.hunger

--- PetGame_MainCode.py
+++ PetGame_MainCode.py
@@ -5,31 +5,30 @@ class Pet:
         self.petName = petName
         self.petType = petType
         self.bored = 0
-        self.hunger = 0
-        self.intelligence = 0
+        self.hunger = 50
+        self.intelligence = 50
 
-    def showHunger():
-        print(self.petName, "has", self.hunger, "hunger points remaining")
+    def showHunger(self, hunger):
         if self.hunger == 100:
             print(self.petName, "has", self.hunger, "hunger points remaining and is full, good job")
-        elif  70 =< self.hunger < 100:
+        elif  70 <= self.hunger < 100:
             print(self.petName, "has", self.hunger, "hunger points remaining and is satisfied")
-        elif 50 =< self.petName < 70:
+        elif 50 <= self.hunger < 70:
             print(self.petName, "has", self.hunger, "hunger points remaining and is getting hungry")
-        elif 20 =< self.petName < 50:
+        elif 20 <= self.hunger < 50:
             print(self.petName, "has", self.hunger, "hunger points remaining and is a little hungry")
-        elif 10 =< self.petName < 20:
+        elif 10 <= self.hunger < 20:
             print(self.petName, "has", self.hunger, "hunger points remaining and is extremely hungry, feed asap")
-        elif 0 < self.petName < 10:
+        elif 0 < self.hunger < 10:
             print(self.petName, "has", self.hunger, "hunger points remaining and will soon starve, feed immediately")
         #include 'dead' here
         else:
-            print("error returning", self.petNme + "'s hunger, sorry :(")
+            print("error returning", self.petName + "'s hunger, sorry :(")
 
-    def outputGreeting():
-        print("hello, i am", self.petName, "i'm a", self.petType)
+    def outputGreeting(self, petName):
+        print("hello, i am", self.petName, "and i'm a", self.petType)
 
 
 newPet = Pet("springy", "tiger")
-newPet.showHunger()
-newPet.outputGreeting()
+newPet.showHunger(newPet.hunger)
+newPet.outputGreeting(newPet.petName)
