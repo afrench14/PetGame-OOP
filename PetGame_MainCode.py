31b8b5937368886969aea81a66c8cdce6740110a
Logan <55439289+afrench14@users.noreply.github.com>
2020-12-03 14:29:58 +0000

    bewan to add showbored method to pet class

--- PetGame_MainCode.py
+++ PetGame_MainCode.py
@@ -8,6 +8,7 @@ class Pet:
         self.hunger = 50
         self.intelligence = 50
         self.alive = True
+        self.entertained = True
 
     #show hunger values
     def showHunger(self, hunger):
@@ -29,7 +30,25 @@ class Pet:
         else:
             print("error returning", self.petName + "'s hunger, sorry :(")
 
-    #def showBored(self, bored):
+    def showBored(self, bored):
+        if self.bored == 0:
+            print(self.petName, "is", self.bored, "percent bored, another good job")
+        elif  0 < self.bored <= 20:
+            print(self.petName, "is", self.bored, "percent bored and is not too un-entertained just yet")
+        elif 20 < self.bored <= 50:
+            print(self.petName, "is", self.bored, "percent bored and will probably get bored some time soon")
+        elif 50 < self.bored <= 70:
+            print(self.petName, "is", self.bored, "hunger points remaining and is a little hungry")
+        elif 10 <= self.bored < 20:
+            print(self.petName, "is", self.bored, "hunger points remaining and is extremely hungry, feed asap")
+        elif 0 < self.bored < 10:
+            print(self.petName, "is", self.bored, "hunger points remaining and will soon starve, feed immediately")
+        elif self.bored == 0:
+            print("unfortunately,", self.petName + "'s bored has reached zero")
+            print("so", self.petName, "has run away to find something more interesting to do")
+            self.entertained = False
+        else:
+            print("error returning", self.petName + "'s bored level, sorry :(")
 
     def outputGreeting(self, petName):
         print("hello, i am", self.petName, "and i'm a", self.petType)
